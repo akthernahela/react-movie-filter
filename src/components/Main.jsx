5c8d2a4eeb76d42351f6d097784a4147eb9e3e5c
akthernahela <nahela00@gmail.com>
2025-11-11 12:10:43 +0100

    Add bootstrap list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -26,5 +26,16 @@ export default function Main() {
         },
     ]
 
-    return ()
+    return (
+        <main>
+            <ul className="list-group list-group-numbered">
+                <li className="list-group-item d-flex justify-content-between align-items-start">
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold">Subheading</div>
+                        Content for list item
+                    </div>
+                </li>
+            </ul>
+        </main>
+    )
 }
