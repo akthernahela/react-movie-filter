1450be66c29ef10e713925d70fb103ed7b30a23d
akthernahela <nahela00@gmail.com>
2025-11-11 12:03:19 +0100

    Add the movie array

---
+++ src/components/Main.jsx
@@ -0,0 +1,30 @@
+export default function Main() {
+    const movies = [
+        {
+            title: 'Inception',
+            genre: 'Fantascienza'
+        },
+        {
+            title: 'Il Padrino',
+            genre: 'Thriller'
+        },
+        {
+            title: 'Titanic',
+            genre: 'Romantico'
+        },
+        {
+            title: 'Batman',
+            genre: 'Azione'
+        },
+        {
+            title: 'Interstellar',
+            genre: 'Fantascienza'
+        },
+        {
+            title: 'Pulp Fiction',
+            genre: 'Thriller'
+        },
+    ]
+
+    return ()
+}
