73de3389ef44152313951979ced67d9be76bdb30
akthernahela <nahela00@gmail.com>
2025-11-11 17:25:08 +0100

    Edit the content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -51,8 +51,8 @@ export default function Main() {
     return (
         <main>
             <label>Scegli per Genere</label>
-            <select class="form-select form-select-sm" aria-label="Small select example">
-                <option selected>Open this select menu</option>
+            <select class="form-select form-select-sm" aria-label="Genre select menu">
+                <option selected>Tutti i Generi</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
